53021d18fcd553a97d0b22a766cf9a8577bea21a
analiacorcino <analia.corcino@gmail.com>
2022-10-10 20:37:26 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,69 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-// create your first component
+
+//ESTE ES EL NUESTROOOOOOOOO//
+
+
+
 const Home = () => {
-  // declaracion de estados
+	//declaracion de estados
 
-  const [tarea, setTarea] = useState("");
-  const [listaDeTareas, setListadeTareas] = useState([]);
+	const [tarea, setTarea] = useState("");
+	const [listaDeTareas, setListaDeTareas] = useState([]);
 
-  function handleSubmit(e) {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      setListadeTareas([...listaDeTareas, tarea]);
-      setTarea("");
-      console.log(listaDeTareas);
-    }
-  }
+	// OnKeyDown
+	const handleKeyDown = (event) => {
+		if (event.key === "Enter") {
+			event.preventDefault();
+			setListaDeTareas([...listaDeTareas, { label: tarea, done: false }]);
+			setTarea("");
+			modificarTarea([...listaDeTareas, { label: tarea, done: false }]);
+			console.log(listaDeTareas);
+		}
+	};
 
-  const eliminarTarea = (i) => {
-    const tareaFiltrada = listaDeTareas.filter((tarea, index) => {
-      if (i !== index) {
-        return tarea;
-      }
-    });
-    setListadeTareas(tareaFiltrada);
-  };
+	const eliminarTarea = (i) => {
+		const tareaFiltrada = listaDeTareas.filter((tarea, index) => {
+			if (i !== index) {
+				return tarea
+			}
+		});
+		setListaDeTareas(tareaFiltrada);
+		modificarTarea(tareaFiltrada);
+	};
 
-  return (
-    <div className="container m-5">
-      <div className="card p-5">
-        <form>
-          <div className="mb-3">
-            <h1>To do List</h1>
+	// PUT
+	const modificarTarea = async () => {// async/await
+		const response = await fetch('https://assets.breatheco.de/apis/fake/todos/user/astrid1', {
+			method: "PUT",
+			body: JSON.stringify([...listaDeTareas, { label: tarea, done: false }]),
+			headers: {
+				'Content-Type': "application/json"
+			}
+		})
+		const data = await response.json();
+		console.log(data);
+	}
 
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              onChange={(e) => setTarea(e.target.value)}
-              onKeyDown={handleSubmit}
-              value={tarea}
-            />
-          </div>
-        </form>
+	// useEffect
+	console.log(listaDeTareas);
+	useEffect(() => {
+		// GET
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/astrid1')
+			.then((response) => response.json())//me convierte la respuesta en json
+			.then((data) => setListaDeTareas(data))//guarda la info json en un objeto
+			.catch((err) => console.log(err))//me avisa si algo salió mal
+	}, [])
 
-        <ul className="list-group list-group-flush">
-          {listaDeTareas.map((tarea, i) => {
-            return (
-              <li className="list-group-item border border-1 tareaOculta" key={i}>
-                {tarea}
-                <span className="close" onClick={() => eliminarTarea(i)}>
-                  x
-                </span>
-              </li>
-            );
-          })}
-        </ul>
-
-        </div> 
-
-        <div className="alert alert-primary mt-5">{listaDeTareas.length == 0 ? "No hay tareas, añadir tareas" : "Numero de tareas: "+listaDeTareas.length}
-      
-      </div>
-    </div>
-  );
+	return (
+		<div className="container bg-primary bg-opacity-10 m-3">
+			<div className="card bg-primary bg-opacity-25">
+				<div className="card-body">
+					<h1 className="card-title d-flex justify-content-center fw-lighter opacity-50">Todos</h1>
+					<form className="container">
+						<div className="mb-3">
+							<input
+								type="text"
+								className="form-control inputt"
+								id="exampleInputTarea"
+								aria-describedby="emailHelp"
+								onChange={(e) => setTarea(e.target.value)}
+								onKeyDown={handleKeyDown}
+								value={tarea}
+							/>
+						</div>
+					</form>
+				</div>
+				<div className="card">
+					<ul className="list-group list-group-flush">
+						{listaDeTareas.map((tarea, i) => {
+							return (<li className="list-group-item border border-1 tareaOculta"
+								key={i}>{tarea.label}<span className="close btn btn-danger" onClick={() => eliminarTarea(i)}><b>X</b></span></li>);
+						})}
+					</ul>
+					<div className="p-1 fw-lighter bg-primary bg-opacity-25"> {listaDeTareas.length == 0 ? "No hay tareas, añade una" : "Numero de tareas: " + listaDeTareas.length}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
